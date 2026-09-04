62b538e44427d48d13c1eaf3ade8b0fc789fcbe7
CPPLoyal <mzbyugioh@gmail.com>
2026-09-03 21:11:18 -0300

    Change video paths and print statements to English
    Updated video file paths and error messages to English.

--- losloslos/src/main.rs
+++ losloslos/src/main.rs
@@ -1,12 +1,19 @@
 use gst::prelude::*;
 use std::io;
 use std::process::Command;
+//the strange way that I give the var names is because I created my own case just for Rust -> FemboyCas
+//it has two ways to write, the first one is to write the letters that are tall n long like L, I, V, U, in upper case while letters like m, n, b, c that doesn't have tall long things are lowercase
+//this thing is because femboys come with a "surprise" together so the long n tall letters represent this and must have a _nya or _uwu ate the end of the name
+
+//the second way is the same thing but we write _ to represent the long striped socks EG: J_U_s_T_nya
+//EG: game_uwu
+//I prefer the first way because is boring to have to long names just because of _
 fn main() {
     gst::init().unwrap();
     loop{
         let mut VIDeo_nya = String::new();
         let mut enD_uwu = false;
-        println!("escolha sua acao");
+        println!("chose");
         if !In_nya(&mut VIDeo_nya, &mut enD_uwu){
             if enD_uwu {
                 break;
@@ -17,12 +24,12 @@ fn main() {
             .name("pLayer_nya")
             .property("uri", VIDeo_nya)
             .build()
-            .expect("Não foi possivel carregar o video");
+            .expect("Coundn't load the video");
         
         let VIDeosYnc_uwu = gst::ElementFactory::make("ximagesink")
             .name("VsYnc_nya")
             .build()
-            .expect("Impossivel criar o sync");
+            .expect("error while making the sync");
         rep_nya.set_property("video-sink", &VIDeosYnc_uwu);
         rep_nya.set_state(gst::State::Playing).unwrap();
         let _ = Command::new("sh")
@@ -44,11 +51,11 @@ fn main() {
             use gst::MessageView;
             match msg_uwu.view(){
                 MessageView::Eos(..) => {
-                    println!("acabou");
+                    println!("end");
                     break;
                 },
                 MessageView::Error(err) => {
-                    println!("Deu não chefia {}", err.error());
+                    println!("Erro: {}", err.error());
                     break;
                 },
                 _ => {},
@@ -61,24 +68,24 @@ fn In_nya(WaY_nya: &mut String, end: &mut bool)->bool{
     let mut nomeesP_nya = String::new();
     io::stdin()
         .read_line(&mut nomeesP_nya)
-        .expect("desconhecido");
+        .expect("Unknown");
     let nome_nya = nomeesP_nya.trim();
     let mut escoLHa_uwu = String::new();
     match nome_nya {
         "losloslos" => {
-            *WaY_nya = "file:///mnt/c/Users/User/Videos/Saga%20of%20Tanya%20the%20Evil%20-%20Ending%20%20%20Los!%20Los!%20Los!.mp4".to_string();
+            *WaY_nya = "file:///ur/directory/Saga%20of%20Tanya%20the%20Evil%20-%20Ending%20%20%20Los!%20Los!%20Los!.mp4".to_string();
             true
         },
         "Ugoku" => {
-            *WaY_nya = "file:///mnt/c/Users/User/Videos/4K%20Video%20Downloader+/ugoku.mp4".to_string();
+            *WaY_nya = "file:///ur/directory/ugoku.mp4".to_string();
             true
         },
         "OneMoreNight" => {
-            *WaY_nya = "file:///mnt/c/Users/User/Videos/4K%20Video%20Downloader+/OneMoreNight.mp4".to_string();
+            *WaY_nya = "file:///ur/directory/OneMoreNight.mp4".to_string();
             true
         },
         "Departure" => {
-            *WaY_nya = "file:///mnt/c/Users/User/Videos/4K%20Video%20Downloader+/Hunter%20X%20Hunter%20Opening%201%20%20%20Departure!.mp4".to_string();
+            *WaY_nya = "file:///ur/directory/Hunter%20X%20Hunter%20Opening%201%20%20%20Departure!.mp4".to_string();
             true
         }
         "list" => {
